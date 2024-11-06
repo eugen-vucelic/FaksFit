c5c775b05b722bae2b0b3c6db3427559d61bd9b0
eugen-vucelic <eugen.vucelic@fer.hr>
2024-11-06 21:26:49 +0100

    Minor annotation fix

--- source_code/backend/src/main/java/com/app/faksfit/model/StudentTerminAssoc.java
+++ source_code/backend/src/main/java/com/app/faksfit/model/StudentTerminAssoc.java
@@ -3,7 +3,7 @@ package com.app.faksfit.model;
 import jakarta.persistence.*;
 
 @Entity
-@Table(name = "PRIJAVLJUJE_TERMIN")
+@Table(name = "PRIJAVLJUJE_TERMIN", schema="FAKSFIT")
 @IdClass(StudentTerminAssocId.class)
 public class StudentTerminAssoc {
 
