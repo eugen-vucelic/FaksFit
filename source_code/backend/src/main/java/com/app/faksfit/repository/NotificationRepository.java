7a7f5cb412ea0b7d242fde6825ee67d0a04ed574
eugen-vucelic <eugen.vucelic@fer.hr>
2024-11-07 14:22:14 +0100

    Created repositories with initial custom queries

---
+++ source_code/backend/src/main/java/com/app/faksfit/repository/NotificationRepository.java
@@ -0,0 +1,11 @@
+package com.app.faksfit.repository;
+
+import com.app.faksfit.model.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    Notification findByNofiticationTitle(String title);
+
+    Notification findByNotificationId(Long notificationId);
+
+}
---
+++ source_code/backend/src/main/java/com/app/faksfit/repository/StudentRepository.java
@@ -0,0 +1,32 @@
+package com.app.faksfit.repository;
+
+import com.app.faksfit.model.Student;
+import com.app.faksfit.model.Teacher;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
+    Student findByJMBAG(String JMBAG);
+
+    List<Student> findByPassStatus(Boolean passStatus);
+
+    List<Student> findBySemester(String semester);
+
+    List<Student> findByTotalPointsGreaterThan(Integer points);
+
+    List<Student> findByTotalPointsBetween(Integer minPoints, Integer maxPoints);
+
+    List<Student> findByStudentTeacher(Teacher teacher);
+
+    @Query("SELECT s FROM Student s JOIN s.terminList t WHERE t.term.termId = :termId")
+    List<Student> findStudentsByTermId(@Param("termId") Long termId);
+
+    boolean existsByJMBAG(String JMBAG);
+
+    List<Student> findByAcademicYearAndTotalPointsGreaterThan(LocalDateTime academicYear, Integer points);
+
+}
