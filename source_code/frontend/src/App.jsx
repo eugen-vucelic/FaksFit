9b9e9365c2d8422ae96dfc6464a283fe33adacf4
vbrebric <brebric.vitomir@gmail.com>
2024-11-07 15:07:48 +0100

    React components structure

--- source_code/frontend/src/App.jsx
+++ source_code/frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import './App.css'
 import {createBrowserRouter, Outlet, RouterProvider} from "react-router-dom";
+import Header from "./components/Header.jsx";
 
 function App() {
 
@@ -18,7 +19,11 @@ export default App
 function AppContainer(props) {
   console.log(props)
   return (
-    <div>
-    </div>
+      <div>
+        <Header/>
+        <div className="App">
+          <Outlet/>
+        </div>
+      </div>
   )
 }
